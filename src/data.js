e7304ece49c90c1fd3914eea59226814ac5d5579
ayush kharya <ayush87269@gmail.com>
2023-10-21 17:03:25 +0530

    addfile

---
+++ src/data.js
@@ -0,0 +1,54 @@
+const data = [
+    {
+        id: 1,
+        name: "Agra",
+        info: "Agra is best known for the Taj Mahal (17th century), designated a UNESCO World Heritage site in 1983. A complex mausoleum, the Taj Mahal is often considered to be the world's best example of Mughal architecture. The Mughal emperor Shah Jahān built it for his favourite wife, Mumtāz Maḥal, in the mid-17th century.",
+        image: "https://lp-cms-production.imgix.net/2019-06/75243789.jpg",
+        price: "5000"
+    },
+    {
+        id: 2,
+        name: "Agra2",
+        info: "Agra is best known for the Taj Mahal (17th century), designated a UNESCO World Heritage site in 1983. A complex mausoleum, the Taj Mahal is often considered to be the world's best example of Mughal architecture. The Mughal emperor Shah Jahān built it for his favourite wife, Mumtāz Maḥal, in the mid-17th century.",
+        image: "https://lp-cms-production.imgix.net/2019-06/75243789.jpg",
+        price: "5000"
+    },
+    {
+        id: 3,
+        name: "Agr3",
+        info: "Agra is best known for the Taj Mahal (17th century), designated a UNESCO World Heritage site in 1983. A complex mausoleum, the Taj Mahal is often considered to be the world's best example of Mughal architecture. The Mughal emperor Shah Jahān built it for his favourite wife, Mumtāz Maḥal, in the mid-17th century.",
+        image: "https://lp-cms-production.imgix.net/2019-06/75243789.jpg",
+        price: "5000"
+    },
+    {
+        id: 4,
+        name: "Agra4",
+        info: "Agra is best known for the Taj Mahal (17th century), designated a UNESCO World Heritage site in 1983. A complex mausoleum, the Taj Mahal is often considered to be the world's best example of Mughal architecture. The Mughal emperor Shah Jahān built it for his favourite wife, Mumtāz Maḥal, in the mid-17th century.",
+        image: "https://lp-cms-production.imgix.net/2019-06/75243789.jpg",
+        price: "5000"
+    },
+    {
+        id: 5,
+        name: "Agra5",
+        info: "Agra is best known for the Taj Mahal (17th century), designated a UNESCO World Heritage site in 1983. A complex mausoleum, the Taj Mahal is often considered to be the world's best example of Mughal architecture. The Mughal emperor Shah Jahān built it for his favourite wife, Mumtāz Maḥal, in the mid-17th century.",
+        image: "https://lp-cms-production.imgix.net/2019-06/75243789.jpg",
+        price: "5000"
+    },
+    {
+        id: 6,
+        name: "Agra6",
+        info: "Agra is best known for the Taj Mahal (17th century), designated a UNESCO World Heritage site in 1983. A complex mausoleum, the Taj Mahal is often considered to be the world's best example of Mughal architecture. The Mughal emperor Shah Jahān built it for his favourite wife, Mumtāz Maḥal, in the mid-17th century.",
+        image: "https://lp-cms-production.imgix.net/2019-06/75243789.jpg",
+        price: "5000"
+    },
+    {
+        id: 7,
+        name: "Agra7",
+        info: "Agra is best known for the Taj Mahal (17th century), designated a UNESCO World Heritage site in 1983. A complex mausoleum, the Taj Mahal is often considered to be the world's best example of Mughal architecture. The Mughal emperor Shah Jahān built it for his favourite wife, Mumtāz Maḥal, in the mid-17th century.",
+        image: "https://lp-cms-production.imgix.net/2019-06/75243789.jpg",
+        price: "5000"
+    },
+
+]
+
+export default data;
